48b4e88fb9630d3fac12aafd423539dbf93ea90a
Gali shalom <shalom.gali.47@gmail.com>
2026-06-19 04:00:57 +0300

    se

--- src/VirtualMemory.cpp
+++ src/VirtualMemory.cpp
@@ -105,16 +105,16 @@ void dfs(uint64_t current_frame, int depth, uint64_t current_page, uint64_t page
         PMread(current_frame * PAGE_SIZE + i, &next_frame);
         if (next_frame != 0) {
             is_empty = false;
-        }
-        uint64_t next_page = (current_page << OFFSET_WIDTH) | i;
+            uint64_t next_page = (current_page << OFFSET_WIDTH) | i;
 
-        dfs(next_frame, depth + 1, next_page, page_swapped_in, frame_to_protect,
-                current_frame, i,
-                out_max_frame_index, out_empty_table_frame, out_empty_parent_frame, out_empty_parent_index,
-                out_max_dist_page, out_evict_frame, out_evict_parent_frame, out_evict_parent_index, out_max_dist);
-    }
+            dfs(next_frame, depth + 1, next_page, page_swapped_in, frame_to_protect,
+                    current_frame, i,
+                    out_max_frame_index, out_empty_table_frame, out_empty_parent_frame, out_empty_parent_index,
+                    out_max_dist_page, out_evict_frame, out_evict_parent_frame, out_evict_parent_index, out_max_dist);
+        }
+        }
     if (is_empty && current_frame != 0 && current_frame != frame_to_protect) {
-        if (out_empty_parent_frame == 0) {
+        if (out_empty_table_frame == 0) {
             out_empty_table_frame = current_frame;
             out_empty_parent_frame = parent_frame;
             out_empty_parent_index = parent_index;
